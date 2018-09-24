2074eda696eb385a07d0023a12113710272e4604
Jimmy <jimmyyuan33@gmail.com>
2018-09-24 16:33:48 -0400

    Add Jimmyzs source file

---
+++ src/Jimmyzs.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Outputs message
+    cout<<"Hello world from Jimmyzs. A fun fact about me is I can speak 3 languages.\n";
+    return 0;
+}
