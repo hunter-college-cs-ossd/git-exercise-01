a5e6791bb262134f84f41f299786294ab1dee3fe
Liudmila Zyrianova <Liudmila.Zyrianova25@myhunter.cuny.edu>
2018-09-24 16:40:48 -0400

    Changed LiudmilaZyrianova239.cpp

--- sources/LiudmilaZyrianova239.cpp
+++ sources/LiudmilaZyrianova239.cpp
@@ -12,11 +12,13 @@
 using namespace std;
 
 
-int main() {
+int main() 
+{
     
 cout<< "Hello world from LiudmilaZyrianova239"<<endl;       //Hello world from  MY_GITHUB_USERNAME
     
 cout<< "One time I was swimming with dolphins in the dolphinarium."<<endl;  //A "fun fact" about myself
     
 return 0;
+
 }
