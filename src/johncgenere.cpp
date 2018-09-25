c0ace50741d9a5ae6f0b9ce22279056e64cd3210
John Genere <johncgenere@gmail.com>
2018-09-25 14:58:23 -0400

    johncgenere's .cpp file with required output

---
+++ src/johncgenere.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // std::cout
+
+int main(){
+  // Output of two sentences
+  std::cout << "Hello world from johncgenere!\n";
+  std::cout << "I find the business side of eSports fascinating.\n";
+  return 0;
+}
