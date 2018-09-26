1845fe7c86dcc052e8a9987feb55831eae740b9d
Francis Irizarry <tuxwonder7@gmail.com>
2018-09-26 00:03:41 -0400

    Updating Info regarding build/ownership

--- src/francisxirizarry.cpp
+++ src/francisxirizarry.cpp
@@ -1,5 +1,16 @@
-
-/*******************************************************************************/
+/*******************************************************************************
+  Title          : francisxirizarry.cpp
+  Author         : Francis X Irizarry
+  Created on     : September 26, 2018
+  Description    : Hello world and a fun fact printed to standard output
+  Purpose        : To collaborate with CS-OSSD group
+  Usage          : Francis X Irizarry
+                   
+  Build with     : g++ -Wall -g -o francisxirizarry francisxirizarry.cpp
+                   ./francisxirizarry
+  Modifications  : 
+ 
+*******************************************************************************/
 #include <iostream>
 using namespace std;
 
