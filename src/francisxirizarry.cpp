bd72b4bb1216b3e359222856c0a0ccba41d33589
francisirizarry <tuxwonder7@gmail.com>
2018-09-25 23:59:04 -0400

    Running program, hello world

---
+++ src/francisxirizarry.cpp
@@ -0,0 +1,15 @@
+
+/*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Hello world from Francis Irizarry.\n";
+    cout << " I don't really interact with people much; however, I am always open to talk.\n";
+    return 0;
+}
+
+
+
+
