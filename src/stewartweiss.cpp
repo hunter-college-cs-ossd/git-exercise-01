c7a9b31c7947a476268ea954c95e75ac17e7a67a
stewartweiss <sweiss@hunter.cuny.edu>
2018-09-25 22:26:20 -0400

    Added my own fun fact to the collection.

---
+++ src/stewartweiss.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+  Title          : stewartweiss.cpp
+  Author         : Stewart Weiss
+  Created on     : September 23, 2018
+  Description    : Hello world and a fun fact printed to standard output
+  Purpose        : To collaborate with CS-OSSD group
+  Usage          : stewartweiss
+                   
+  Build with     : g++ -Wall -g -o stewartweiss  stewartweiss.cpp
+  Modifications  : 
+ 
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Hello world from stewartweiss.\n";
+    cout << " I was one of the half million who went to Woodstock in 1969.\n";
+    return 0;
+}
