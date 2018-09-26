05bf8b6ccb2d9ea33b3b0f189ba804212ecceda6
John Genere <johncgenere@gmail.com>
2018-09-26 16:54:08 -0400

    Added Author and Date of Creation

--- src/johncgenere.cpp
+++ src/johncgenere.cpp
@@ -1,3 +1,10 @@
+/*
+ Author: John Genere
+ Created On: September 25th, 2018
+ 
+ This program outputs two sentences, one contains a fun fact about me.
+*/
+
 #include <iostream>  // std::cout
 
 int main(){
