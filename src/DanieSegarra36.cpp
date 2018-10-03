df9957430315000349817605e41c550cad40147c
Daniel Segarra <danielsegarra36@gmail.com>
2018-10-03 01:40:16 -0400

    Uploaded assignment.

---
+++ src/DanieSegarra36.cpp
@@ -0,0 +1,17 @@
+/*
+  Author: Daniel Segarra
+  Course: CSCI 393.02 - Open Source Software
+  Instructor: Stewart Weiss
+  Assignment: Git Work􏰂flow
+    The purpose of this assignment was to familiarize the student with git commands.
+    In addition, the student had to learn and understand
+    how pushing and pulling works on forked branches, in relation to the original repo.
+    The exercise was then concluded with the final C++ code here.
+    Commiting this file will provide the student an example of pushing to repos.
+*/
+
+#include <iostream> // for output to console
+int main(){
+  std::cout << "Hello world from DanieSegarra36\n";
+  std::cout << "A fun fact about me is that I'm obsessed with my cat. Her name is Arya, like the Game of Thrones Character! :D\n";
+}
